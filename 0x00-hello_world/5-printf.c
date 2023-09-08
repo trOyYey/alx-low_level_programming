2d3846179ccd7ff2ff3984d848289651be0f531b
trOyYey <troyzuwara@gmail.com>
2023-09-07 19:44:13 -0700

    fixing betty style

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
