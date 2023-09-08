09d90bf322344b9e2ed4e01bd873497098244e1b
trOyYey <troyzuwara@gmail.com>
2023-09-07 19:32:38 -0700

    fixing spaces and adding description in C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descriptions : A C program that can print with put function
+ *
+ * Return: Akways 0 (Success)
+ */
+
+int main(){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
