03382059eb3f16a0118ebbeb86ff38e2ca77cea1
trOyYey <troyzuwara@gmail.com>
2023-09-08 19:10:39 -0700

    positive and negative check task0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/*
+ * main: Entry Point
+ *
+ * Description: check if the input number is positive or negative
+ *
+ * Return: Always 0(Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printif("%d is zero\n", n);
+	}
+	else 
+	{
+		printf("%d is negative\n", n):
+	}
+	return (0);
+}
