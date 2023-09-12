2960dfb24f34310ce5a9ebefe28d972407add30a
trOyYey <troyzuwara@gmail.com>
2023-09-11 19:54:35 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description:
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int firstDigit = 0, seconDigit;
+
+	while (firstdigit <= 99)
+	{
+		secondDigit = firstDigit;
+		while (seconDigit <= 99)
+		{
+			if (seconDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((seconDigit / 10) + 48);
+				putchar((seconDigit % 10) + 48);
+
+				if (firstDigit != 98 || seconDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			seconDigit++;
+		}
+		firstDigit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
