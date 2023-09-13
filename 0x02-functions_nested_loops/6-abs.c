14b550931ce71f4ce026abd3d0dc7680aee03878
trOyYey <troyzuwara@gmail.com>
2023-09-12 19:58:19 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,14 @@
 
 /**
  * _abs - function that computes the absolute
- * 		value of an integer
+*		value of an integer
  * @n: takes an integer type input for function
  * Return: Always 0
  */
 
 int _abs(int n)
 {
-	if (n <0)
+	if (n < 0)
 		n = (-1) * n;
 	return (n);
 }
