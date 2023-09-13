395ce47d1a7bb37f157c66d2846d3236882cc399
trOyYey <troyzuwara@gmail.com>
2023-09-12 22:22:01 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,15 +2,16 @@
 
 /**
  * jack_bauer - function that prints every minute of the day
+*		can use 4 int and 4 loops or 2 int and 2 lopps
  */
 
 void jack_bauer(void)
-{   //we can either use 2 int or 4,it's easier to understand with 4
-	// but it takes less lines with only 2
+{
 	int min, hr;
+
 	for (hr = 0; hr <= 23; hr++)
 	{
-		for (min = 0; min <=59; min++)
+		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
