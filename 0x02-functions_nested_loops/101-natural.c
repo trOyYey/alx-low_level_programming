63366b7a1e07c08630010e23a179de88287e2dcf
trOyYey <troyzuwara@gmail.com>
2023-09-13 19:38:57 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: computes the sum of all multi of 3 and 5
+ * Return: Always 0 on success
+ */
+
+int main(void)
+{
+	int sum, numb;
+
+	for (numb = 0; numb < 1024; numb++)
+	{
+		if ((numb % 3 == 0) || (num % 5 == 0))
+			sum += numb;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
