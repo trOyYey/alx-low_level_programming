6ab9e56764bba02b77d8eaffb4a84736a420ef77
trOyYey <troyzuwara@gmail.com>
2023-09-15 16:55:57 -0700

    ./8-squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,10 +9,15 @@ void print_square(int size)
 {
 	int r, c;
 
-	for (r = 1; r <= size; r++)
-	{
-		for (c = 1; c <= size; c++)
-			_putchar('#');
+	if (size == 0)
 		_putchar('\n');
+	else
+	{
+		for (r = 1; r <= size; r++)
+		{
+			for (c = 1; c <= size; c++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
