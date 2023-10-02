a2bf29e317b2c428819a5b4d788f8ce33fb0fce2
trOyYey <troyzuwara@gmail.com>
2023-10-02 15:35:13 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: int
+ * @argv: pointer of string of arrays
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[]);
+	return (0);
+}
