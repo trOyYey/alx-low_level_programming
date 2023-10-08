d258ed15f9dd4e81d196e5d881d3c53af25134ff
trOyYey <troyzuwara@gmail.com>
2023-10-08 16:57:21 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - function that allocates memory
+ * @b: integer
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *m;
+
+	*m = malloc(b);
+	if (m == 0)
+		exit(98);
+	return (m);
+}
