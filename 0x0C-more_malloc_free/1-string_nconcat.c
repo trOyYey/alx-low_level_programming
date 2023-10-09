e1aba3cd5a21ce8d48afca180c95317b23c798fe
trOyYey <troyzuwara@gmail.com>
2023-10-08 18:14:30 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-string_nconcat.c -o 1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * char *string_nconcat - function that concatenates two strings
@@ -10,23 +11,23 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, s1L = 0, s2L = 0;
+	unsigned int i, s1L = 0, s2L = 0;
 
-	char *P
+	char *P;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1L[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		s1L++;
-	for (i = 0; s2L[i] != '\0'; i++)
+	for (i = 0; s2[i] != '\0'; i++)
 		s2L++;
 
 	P = malloc(sizeof(char) * (s1L + n) + 1);
 	if (P == NULL)
 		return (NULL);
-	if (p >= n)
+	if (n >= s2L)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			P[i] = s1[i];
@@ -42,5 +43,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			P[s1L + i] = s2[i];
 		P[s1L + i] = '\0';
 	}
+	return (P);
 }
 
