2eadbf6cb8ea976bb19f9fa7dd5c6fc5869e2d1e
trOyYey <troyzuwara@gmail.com>
2023-10-08 18:45:24 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *P;
 
-	if ( nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	P = calloc(nmemb, size);
 	if (P == NULL)
