994e1b5bcbea7531694579c391b1f391c4d80ab8
trOyYey <troyzuwara@gmail.com>
2023-10-10 18:48:01 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defininning new type struct dog
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ * Description: this is my cat
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
