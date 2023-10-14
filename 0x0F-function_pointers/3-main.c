c8a3511bb9c2d862485ea45ccd14dc352b67715d
trOyYey <troyzuwara@gmail.com>
2023-10-13 19:34:50 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * main - argc argv type function
+ * @argc: number of arguments
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int (*op_func)(int, int), a, b;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+
+	if (!op_func)
+		printf("Error\n"), exit (99);
+
+	if (!b && (argv[2][0] == '/' || agrv[2][0] == '%'))
+		printf("Error\n"), exit(100);
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/op_functions.c
@@ -0,0 +1,50 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two int
+ * @a: int
+ * @b: int
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: the first int
+ * @b: the second int
+ * Return: the difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div - divides two int
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulu of two int
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
