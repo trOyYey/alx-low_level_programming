32d0ba945ba9cc2d8e942ca9e43a977c0b842fc0
trOyYey <troyzuwara@gmail.com>
2023-10-19 12:29:16 -0700

    making readme and fixing new line

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -27,7 +27,7 @@ size_t print_list(const list_t *h)
 	for (i = 0; h != NULL; i++, h = h->next)
 	{
 		if (h->str == NULL)
-			printf("[0] (nill)");
+			printf("[0] (nill)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 	}
