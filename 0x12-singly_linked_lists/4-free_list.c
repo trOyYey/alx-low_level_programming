3528e4987da9c982cec5669a6c416636cf0a285c
trOyYey <troyzuwara@gmail.com>
2023-10-19 14:08:16 -0700

    free task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_list - frees list_t list 
+ * @head: pointer to head
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *i = head;
+
+	while (i)
+	{
+		i = i->next;
+		free(head->str);
+		free(head);
+		head = i;
+	}
+}
