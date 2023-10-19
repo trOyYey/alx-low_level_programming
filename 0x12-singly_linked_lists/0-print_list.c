32025e2a57733ae8e9cac54a7ece6713943be7ea
trOyYey <troyzuwara@gmail.com>
2023-10-19 12:31:03 -0700

    deleting one l from nil

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -27,7 +27,7 @@ size_t print_list(const list_t *h)
 	for (i = 0; h != NULL; i++, h = h->next)
 	{
 		if (h->str == NULL)
-			printf("[0] (nill)\n");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 	}
