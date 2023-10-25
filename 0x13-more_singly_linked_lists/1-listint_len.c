29fdd95d9338655ff8dfbf618711970fe0ce9afc
trOyYey <troyzuwara@gmail.com>
2023-10-24 18:13:05 -0700

    task01

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - counts number of elements in a linked list
+ * @h: pointer to listint_t
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
