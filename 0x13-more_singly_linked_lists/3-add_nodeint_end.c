fe023088e5858dac51d18f90c8678577a2c1cf9c
trOyYey <troyzuwara@gmail.com>
2023-10-24 18:32:26 -0700

    task03

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *end_node = malloc(sizeof(listint_t));
-	listint_t tmp_node;
+	listint_t *tmp_node;
 
 	if (!head || !end_node)
 		return (NULL);
