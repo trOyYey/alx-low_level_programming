c45f610c474c7df57dfe1b92e3a2bdf9abb2ce05
trOyYey <troyzuwara@gmail.com>
2023-10-24 19:30:12 -0700

    task 08

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,16 +6,6 @@
  * Return: sum
  */
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int sum_listint(listint_t *head)
 {
 	int result = 0;
