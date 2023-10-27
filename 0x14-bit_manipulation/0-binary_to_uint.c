0df4b1c8b1fb5e8796ac51f4bb4a3081e0d1e7d9
trOyYey <troyzuwara@gmail.com>
2023-10-26 19:25:06 -0700

    task number 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	while (b[i])
 	{
 		bin = bin << 1;
-		if (*b[i] > '9' && *b[i] < '0')
+		if (b[i] > '9' && b[i] < '0')
 			return (0);
 		bin = bin + (b[i] - '0');
 		i++;
