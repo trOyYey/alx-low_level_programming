1c7535d60b1aac4cd06ac5762d86b9cf62971817
trOyYey <troyzuwara@gmail.com>
2023-10-30 19:09:32 -0700

    fixing betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	cl = open(filename, O_WRONLY |O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	cl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (cl == -1)
 		return (-1);
 	if (len)
