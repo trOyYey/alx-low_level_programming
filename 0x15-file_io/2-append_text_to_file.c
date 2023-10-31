f522068ad27a6ca12ec60f1df60b79df88b9c388
trOyYey <troyzuwara@gmail.com>
2023-10-30 19:17:05 -0700

    task02

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - function that calculates the lenght of string
+ * @s: string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * append_text_to_file - as name suggests
+ * @filename: const char
+ * @text_content: text
+ * Return: 1 on succ or 0 in failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
