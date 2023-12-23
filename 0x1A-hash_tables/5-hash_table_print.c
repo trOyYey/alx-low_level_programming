00e489ba69b8a21ebdc6b72205afd4369dc278ea
trOyYeyY <troyzuwara@gmail.com>
2023-12-23 01:01:46 +0000

    printing hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints hash table
+ * @ht: ponter to the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int x;
+	int p = 0;
+
+	if (ht == NULL)
+		return ;
+	putchar('{');
+	for (x = 0; x < ht->size; x++)
+	{
+		if (ht->array[x])
+		{
+			print_hash_node(ht->array[x], &p);
+		}
+	}
+	putchar('}');
+	putchar('\n');
+}
+
+/**
+ * print_node - prints hash nodes
+ * @head: ponter to linked list
+ * @p: pointer
+ * Return: 0 if succ 1 otherwise
+ */
+
+void print_hash_node(hash_node_t *head, int *p)
+{
+	hash_node_t *clone;
+
+	for (clone = head; clone; clone = clone->next)
+	{
+		if (*p)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			*p = 1;
+		printf("'%s': '%s'", clone->key, clone->value);
+	}
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -44,5 +44,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 int add_hash_node(hash_node_t **head, const char *key, const char *value);
+void print_hash_node(hash_node_t *head, int *p);
 
 #endif
