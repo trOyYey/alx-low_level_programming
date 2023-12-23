e71598361347b4f26d854c7a9db7942a31f7e546
trOyYeyY <troyzuwara@gmail.com>
2023-12-23 00:49:29 +0000

    fixing typo in get function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || strlen(key) == 0)
 		return (0);
 	x = key_index((const unsigned char *)key, ht->size);
-	if (add_hash_node(&ht->array[i], key, value))
+	if (add_hash_node(&ht->array[x], key, value))
 		return (0);
 	return (1);
 }
