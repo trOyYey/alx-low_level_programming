603e0823cdc59c8713e2d0de48b644f10a4aac1c
trOyYeyY <troyzuwara@gmail.com>
2024-04-12 01:44:24 +0000

    adding header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+void print_list(int *array, size_t start, size_t end);
+int recursive_binary(int *array, size_t first, size_t end, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
