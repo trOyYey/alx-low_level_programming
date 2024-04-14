b2a0d319bb594ea40cc377cc4c35ca2045923bf7
trOyYeyY <troyzuwara@gmail.com>
2024-04-14 19:41:00 +0000

    jump_list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,21 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 int linear_search(int *array, size_t size, int value);
 void print_list(int *array, size_t start, size_t end);
 int recursive_binary(int *array, size_t first, size_t end, int value);
@@ -13,5 +28,10 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binrecur(int *array, size_t first, size_t last, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *iterate_list(listint_t *list, size_t step);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+void print_list_2(const listint_t *list);
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
 
 #endif
