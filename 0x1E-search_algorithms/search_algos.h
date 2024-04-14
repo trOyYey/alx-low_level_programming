1a15d0fab577252d82e015a17fa834a6fc31ee29
trOyYeyY <troyzuwara@gmail.com>
2024-04-14 13:08:32 +0000

    jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,10 +2,12 @@
 #define HEADER_FILE
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 void print_list(int *array, size_t start, size_t end);
 int recursive_binary(int *array, size_t first, size_t end, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
